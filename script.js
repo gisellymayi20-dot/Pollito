c1e8c29ad924f13c4f6b548d962a834fe20a8a2a
gisellymayi20-dot <gisellymayi20@gmail.com>
2026-03-13 15:57:59 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 const gifStages = [
-    "https://media.tenor.com/EBV7OT7ACfwAAAAj/u-u-qua-qua-u-quaa.gif",    // 0 normal
-    "https://media1.tenor.com/m/uDugCXK4vI4AAAAd/chiikawa-hachiware.gif",  // 1 confused
-    "https://media.tenor.com/f_rkpJbH1s8AAAAj/somsom1012.gif",             // 2 pleading
-    "https://media.tenor.com/OGY9zdREsVAAAAAj/somsom1012.gif",             // 3 sad
-    "https://media1.tenor.com/m/WGfra-Y_Ke0AAAAd/chiikawa-sad.gif",       // 4 sadder
-    "https://media.tenor.com/CivArbX7NzQAAAAj/somsom1012.gif",             // 5 devastated
-    "https://media.tenor.com/5_tv1HquZlcAAAAj/chiikawa.gif",               // 6 very devastated
-    "https://media1.tenor.com/m/uDugCXK4vI4AAAAC/chiikawa-hachiware.gif"  // 7 crying runaway
+    "https://media.tenor.com/al0fps7Hek0AAAAj/bird-cute.gif",    // 0 normal
+    "https://media.tenor.com/_Aubf4KDTfIAAAAj/chicken-love.gif",  // 1 confused
+    "https://media.tenor.com/_SekInP13ZsAAAAi/piyopiyo-baby-chicken.gif",             // 2 pleading
+    "https://media.tenor.com/xMF3v6oD2uUAAAAj/kokoriiland-bok-bok-the-chicken.gif",             // 3 sad
+    "https://media1.tenor.com/m/eUsZNIAl7wYAAAAC/crying-piu-piu.gif",       // 4 sadder
+    "https://media.tenor.com/idVOO_SBA30AAAAi/sad-cry.gif",             // 5 devastated
+    "https://media1.tenor.com/m/jzZFSJfl0ZEAAAAd/eee.gif",               // 6 very devastated
+    "https://media.tenor.com/eLJlHkRCmBMAAAAj/akirambow-plump-little-chick.gif"  // 7 crying runaway
 ]
 
 const noMessages = [
